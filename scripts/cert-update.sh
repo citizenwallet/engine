39aafedc5df746b2f8134de65f5ed767d141f219
kev-techi <kevin@techi.be>
2025-08-14 13:27:12 +0200

    force cert renewal

--- scripts/cert-update.sh
+++ scripts/cert-update.sh
@@ -3,6 +3,6 @@
 DOMAIN=$1
 
 cd $HOME/engine
-docker compose run --rm certbot-renew certonly --non-interactive --webroot -w /var/www/certbot -d $DOMAIN
+docker compose run --rm certbot-renew certonly --non-interactive --webroot -w /var/www/certbot -d $DOMAIN --force-renewal
 
 docker compose restart server
